67f7d8343313b30c58ac545b1df5e1a47e1eb9f2
nigh7m4r3 <rifatulchayon@gmail.com.com>
2014-09-22 00:45:50 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(
+  
+  /* This is the function that will get executed after the DOM is fully loaded */
+  function () {
+    $( "#datepicker" ).datepicker({
+      changeMonth: true,//this option for allowing user to select month
+      changeYear: true //this option for allowing user to select from year range
+    });
+
+     $( "#datepicker2" ).datepicker({
+      changeMonth: true,//this option for allowing user to select month
+      changeYear: true //this option for allowing user to select from year range
+    });
+  }
+
+);
